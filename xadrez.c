7eed7705f84bada487748cb49164b9cbf7545286
EricMart-Pro <ericgaidargimartins@gmail.com>
2025-09-10 14:36:07 +0000

    Criei variavéis e movimentação das peças.

--- xadrez.c
+++ xadrez.c
@@ -8,14 +8,47 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    int opcao;
+    int Bispo = 1;
+    int Torre = 1;
+    int Rainha = 1;
+
+    printf("### Jogo de Xadrez ###\n");
+    printf("Escolha qual peça deseja movimentar:\n");
+    printf("1. Bispo\n");
+    printf("2. Torre\n");
+    printf("3. Rainha\n");
+    printf("Digite o número correspondente a peça: ");
+    scanf("%d", &opcao);
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    if(opcao == 1) {
+    while (Bispo <= 5) {
 
+        
+        printf("Cima, Direita\n");
+        Bispo++;
+
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+} else if (opcao == 2) {
 
+    do{
+        
+        printf("Direita\n");
+        Torre++;
+        
+    }while (Torre <= 5);
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+} else if(Rainha <= 8) {
+
+    for(Rainha = 1; Rainha <= 8; Rainha++) {
+        printf("Esquerda\n");
+    }
+}
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
